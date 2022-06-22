ac9c600df8f90ea8aeab2f92d7350dacf9454964
侯智洋 <houzhiyang1997@163.com>
2022-06-22 21:27:28 +0800

    后台管理-用户信息删除api

--- index.js
+++ index.js
@@ -141,6 +141,28 @@ router.post('/admin/edituser', async ctx => {
   }
 })
 
+// 根据id删除用户信息
+router.get('/admin/deleteuser', async ctx => {
+  ctx.status = 200
+  const _info = ctx.query
+  try {
+    let _sql = 'DELETE FROM user WHERE id=?'
+    let _value = [_info.id]
+    let _data = await poolSql(_sql, _value)
+    ctx.body = {
+      errorMessage: '',
+      result: true,
+      count: _data.affectedRows
+    }
+  } catch (error) {
+    ctx.body = {
+      errorMessage: '删除用户详情失败',
+      result: false,
+      count: null
+    }
+  }
+})
+
 // 获取全部新闻列表
 router.get('/getnews', async ctx => {
   ctx.status = 200
