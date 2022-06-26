1239c58087fadfc3fbb61976018b53a6a00c01a3
侯智洋 <houzhiyang1997@163.com>
2022-06-26 21:04:04 +0800

    根据id删除阵容api

--- index.js
+++ index.js
@@ -1603,6 +1603,27 @@ router.get('/admin/deletehero', async ctx => {
   }
 })
 
+// 根据id删除 阵容team 信息
+router.get('/admin/deleteteam', async ctx => {
+  ctx.status = 200
+  const _info = ctx.query
+  try {
+    let _sql = 'DELETE FROM team WHERE id=?'
+    let _value = [_info.id]
+    let _data = await poolSql(_sql, _value)
+    ctx.body = {
+      errorMessage: '',
+      result: true,
+      count: _data.affectedRows
+    }
+  } catch (error) {
+    ctx.body = {
+      errorMessage: '删除阵容team失败',
+      result: false,
+      count: null
+    }
+  }
+})
 /* 
 
 
